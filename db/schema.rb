ba4a5bcc6baef6777369a4edc315ad631f103e73
Arricks <kabata.yuya@gmail.com>
2020-05-21 07:06:28 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_18_133357) do
+ActiveRecord::Schema.define(version: 2020_05_21_070209) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "end_user_id"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2020_05_18_133357) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.boolean "is_deleted", default: false, null: false
     t.index ["email"], name: "index_end_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
   end
