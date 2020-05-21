b953bff11391c8e35e24b4d71ca54cea04cf0a81
Arricks <kabata.yuya@gmail.com>
2020-05-21 05:45:57 +0000

    change views

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '52527857701f4b0c132f05b978bc0c9fc5b12f5e41c9bbca44fde8ca8254dffc8e1d3d4c469032de07afc80579701a4d396c389c8e060c04fbb50b6f0fef8397'
