782b3df886b00c0a1e39891c10b6bc53403e845f
Arricks <kabata.yuya@gmail.com>
2020-05-25 16:19:06 +0000

    something

---
+++ db/migrate/20200514135639_create_end_users.rb
@@ -0,0 +1,15 @@
+class CreateEndUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :end_users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :first_kana_name
+      t.string :last_kana_name
+      t.string :postcode
+      t.string :address
+      t.string :phone_number
+
+      t.timestamps
+    end
+  end
+end
