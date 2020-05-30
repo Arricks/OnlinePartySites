5a1cf1636dfd5bc811dcaf7b437e2c5775cdd807
Arricks <kabata.yuya@gmail.com>
2020-05-30 02:48:12 +0000

    add is_deleted_column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_143258) do
+ActiveRecord::Schema.define(version: 2020_05_30_024606) do
 
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_05_27_143258) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.boolean "is_deleted", default: false, null: false
     t.index ["email"], name: "index_end_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
   end
